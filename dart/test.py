fe45ae0b99d2a31c5690c4bd5caa1e36b39c2262
diligejy <sjy049@gmail.com>
2020-06-23 01:03:21 +0900

    Bytes IO Convert to csv

--- dart/test.py
+++ dart/test.py
@@ -1,4 +1,6 @@
 import requests
-
-resp = requests.get('https://en.wikipedia.org/wiki/Computing')
+url = 'http://dart.fss.or.kr/pdf/download/excel.do?rcp_no=20200515002179&dcm_no=7343256&lang=ko'
+user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.120 Safari/537.36"
+resp = requests.get(url, headers = {"user-agent": user_agent})
 print(resp)
+print(resp.content)
